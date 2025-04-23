a90dcbc62da5c72fb90b571cd384b4c467c3f97d
Akatsuki667 <qextzy@gmail.com>
2025-04-23 15:20:41 +0200

    updating README.md, 1-concurrent_coroutines.py

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 """
-Module fonction affichage nb aléatoire
+Module qui définit une fonction asynchrone qui attend un délai aléatoire.
 """
 import asyncio
 import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """
-    Retourne le nb alétoire avec du délai
+    Attend pendant une durée aléatoire entre 0 et max_delay secondes
+    puis retourne cette durée.
+    
+    Args:
+        max_delay (int, optional): Délai maximum d'attente en secondes. 
+        Valeur par défaut: 10.
+    
+    Returns:
+        float: La durée d'attente en secondes.
     """
     wait = random.uniform(0, max_delay)
     await asyncio.sleep(wait)
---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Module qui implémente une fonction asynchrone wait_n qui exécute
+plusieurs coroutines concurrentes et retourne leurs résultats dans
+l'ordre de complétion.
+"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Exécute n fois wait_random(max_delay) de façon concurrente
+    et retourne les délais dans l'ordre de complétion.
+    
+    Args:
+        n (int): Nombre de fois à exécuter wait_random.
+        max_delay (int): Délai maximum possible (en secondes) pour chaque attente.
+        
+    Returns:
+        List[float]: Liste des délais d'attente dans l'ordre de complétion.
+    """
+    list = []
+    #  Génération n coroutine
+    coroutine = [wait_random(max_delay) for _ in range(n)]
+    #  Traitement des résultats dans l'ordre de complétion
+    for task in asyncio.as_completed(coroutine):
+        #  Pour chaque task terminé:
+        #  Attends résultat avec wait
+        result = await task
+        #  Ajoute à la liste
+        list.append(result)
+    return list

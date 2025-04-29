143c7692af4841d88334a70d2513e7fd76d7ccc4
Akatsuki667 <qextzy@gmail.com>
2025-04-29 09:41:48 +0200

    refactoring code

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -11,18 +11,12 @@ async def measure_runtime() -> float:
     """
     Mesure temps d'exécution pour quatre appels à async_comprehension.
 
-    Cette fonction crée 4 tâches asynchrones exécutant async_comprehension,
-    les exécute en parallèle avec asyncio.gather() et mesure le temps total
-    d'exécution.
-
     Returns:
     float: Le temps total d'exécution en secondes.
     """
-    coroutines = []
-    for _ in range(4):
-        result = async_comprehension()
-        coroutines.append(result)
     start = perf_counter()
-    await asyncio.gather(*coroutines)
+    #  Création 4 appels async_comprehension()
+    #  * unpacks l'expression génératrice que appel = argument
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
     end = perf_counter()
-    return (end - start)
+    return end - start

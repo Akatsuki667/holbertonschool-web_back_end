e29d780a9c8b3657627bdca03cc3197c142dc84c
Akatsuki667 <qextzy@gmail.com>
2025-05-21 10:25:13 +0200

    updating README.md, adding -stdin.js

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,12 @@
+// Mode "flowing"
+process.stdin.setEncoding('utf8');
+
+console.log('Welcome to Holberton School, what is your name?');
+
+// Écouter l'événement
+process.stdin.on('data', (data) => {
+  const name = data.trim(); // Supprime retours à la ligne
+  console.log('Your name is : ', name);
+  console.log('This important software is now closing\n');
+  process.exit() // Quitter le programme
+})
